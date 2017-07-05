fa7f9cfce23899c9578be11ae0a9a0d91f4df5ca
Joe Tessy <josephctessy@gmail.com>
2017-07-05 17:20:17 -0400

    Hooked up audio!

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,15 @@
-let audioContext = null;
+window.AudioContext = window.AudioContext || window.webktAudioContext;
+let audioContext = new AudioContext();
+let source = null,
+    gainNode = null,
+    inputPoint = null;
+
+const onOff = document.querySelector('#on-off');
+const audioOn = document.querySelector('.audio-on');
+const audioOff = document.querySelector('.audio-off');
 
 function gotStream(stream){
-  window.AudioContext = window.AudioContext || window.webktAudioContext;
-  audioContext = new AudioContext();
-
-  let mediaStreamSource = audioContext.createMediaStreamSource(stream);
-  let gainNode = audioContext.createGain();
-  mediaStreamSource.connect(audioContext.destination);
-
+  source = audioContext.createMediaStreamSource(stream);
   console.log('Hooray, audio is connected!');
 }
 
@@ -18,12 +20,37 @@ function didntGetStream(){
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
 navigator.getUserMedia({audio: true}, gotStream, didntGetStream);
 
+onOff.onclick = startStopAudio;
 
-function soundOff(){
 
+function startStopAudio(){
+  onOff.classList.toggle('audio-off');
+  if (gainNode === null || gainNode.gain.value === 0) {
+    gainNode = audioContext.createGain();
+    gainNode.gain.value = .5;
+    source.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    onOff.innerHTML='ON';
+  } else {
+    gainNode.gain.value = 0;
+    onOff.innerHTML='OFF';
+    gainNode = null;
+  }
 }
 
-function soundOn(){
-
-
+function setVolume(volume){
+  gainNode.gain.value = volume;
 }
+
+$('#volume').slider({
+  orientation: 'vertical',
+  range: 'min',
+  min: 0,
+  max: 100,
+  value: 50,
+  animate: true,
+  slide: function(event, ui){
+    if (gainNode)
+    setVolume((ui.value) / 100);
+  }
+});
